b659c9c07e944f3105c05e002198eb7103aa4022
mins0619 <mins5588@naver.com>
2020-12-09 01:06:38 +0900

    게시판 구현 완료 ejs 문법 적용

--- Danawa/app.js
+++ Danawa/app.js
@@ -100,12 +100,12 @@ app.use((req, res, next) => {
   next(error);
 });
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use((err, req, res, next) => {
+//   res.locals.message = err.message;
+//   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
 });
